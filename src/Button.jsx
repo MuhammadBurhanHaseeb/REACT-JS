b818dc5f462b0a9bbecb5d06253a30663a3b9584
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-08-20 21:48:17 +0500

    Button

---
+++ src/Button.jsx
@@ -0,0 +1,16 @@
+function Button() {
+    // inline styles
+    // const styles ={
+    //     borderRadius: "8px",
+    //     border: "10px solid transparent",
+    //     padding: "0.6em 1.2em",
+    //     fontSize: "1em",
+    //     fontWeight: "500",
+    //     fontFamily: "inherit",
+    //     backgroundColor: "#1a1a1a",
+    //     cursor: "pointer",
+    //     transition: "borderColor 0.25s",
+    // }
+  return <button styles={styles}>Click me</button>;
+}
+export default Button;
