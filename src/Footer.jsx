039eb76759a9b613f65a3714b69ab695449139a0
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-08-20 21:46:18 +0500

    Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function footer () {
+    return (
+        <footer>
+            <p>© {new Date().getFullYear() } 2021 My React App</p>
+            <hr />
+        </footer>
+    );
+
+}
+export default footer;
